1211160d1a39f574ae7a9d956802b6003077e78a
Romain Segarizzi <romain.segarizzi@hotmail.fr>
2025-06-05 15:29:25 +0200

    Finalisation de la configuration de la page admin

--- src/Controller/Admin/JobApplicationCrudController.php
+++ src/Controller/Admin/JobApplicationCrudController.php
@@ -4,7 +4,10 @@ namespace App\Controller\Admin;
 
 use App\Entity\JobApplication;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
@@ -15,14 +18,19 @@ class JobApplicationCrudController extends AbstractCrudController
         return JobApplication::class;
     }
 
-    /*
+    
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            IdField::new('id')->hideOnForm(),
+            TextareaField::new('coverLetter')
+                ->setLabel('Cover Letter')
+                ->setRequired(true),
+            DateTimeField::new('createdAt'),
+            AssociationField::new('job')->autocomplete(),
+            AssociationField::new('user')->autocomplete(),
+
         ];
     }
-    */
+    
 }
--- src/Controller/Admin/JobCrudController.php
+++ src/Controller/Admin/JobCrudController.php
@@ -4,7 +4,10 @@ namespace App\Controller\Admin;
 
 use App\Entity\Job;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
@@ -15,14 +18,23 @@ class JobCrudController extends AbstractCrudController
         return Job::class;
     }
 
-    /*
+    
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
+            IdField::new('id')->hideOnForm(),
             TextField::new('title'),
-            TextEditorField::new('description'),
+            TextareaField::new('description')->setSortable(true),
+            TextField::new('city'),
+            TextField::new('country'),
+            TextField::new('remoteAllowed'),
+            TextField::new('salaryMin'),
+            TextField::new('salaryMax'),
+            DateTimeField::new('createdAt'),
+            AssociationField::new('company')->autocomplete(),
+            AssociationField::new('jobType')->autocomplete(),
+
         ];
     }
-    */
+    
 }
